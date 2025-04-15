471fa726be7dabe35c4e518d1342ce8a5d765e8b
Sam Brownstone <brownstone@hey.com>
2025-04-15 10:52:50 +0100

    Add aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -10,9 +10,19 @@ alias gco='git checkout'
 alias gcb='git checkout -b'
 alias gs='git status'
 alias gp='git push'
+alias gpl='git pull'
 alias gpf='git push --force-with-lease'
+alias grc='git rebase --continue'
 
 alias mr='mise run'
 alias ms='mise registry | rg'
 
+alias n='npm'
+alias ni='npm install'
+
+alias p='pnpm'
+alias pi='pnpm install'
 alias pnpx='pnpm dlx'
+
+alias v='nvim'
+
